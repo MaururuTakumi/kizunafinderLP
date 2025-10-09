665a950376ca3d8a88449d29be220b082a1fdde8
toofat2003 <136313842+toofat2003@users.noreply.github.com>
2025-10-09 15:47:39 +0900

    added terms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,6 @@ export default function Home() {
         <Hero />
         <Strengths />
         <Steps />
-        <CompareTable />
         <NowNext />
         <Templates />
         <BetaSection />
